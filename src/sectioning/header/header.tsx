128d3050c00efd5e3429569fde3afea6d3e61532
Matthew Day <matt@matthewday.net>
2024-02-17 18:42:00 -0500

    add navs to header via JSON import

--- src/sectioning/header/header.tsx
+++ src/sectioning/header/header.tsx
@@ -1,5 +1,12 @@
-export const Header = (): JSX.Element => (
-  <header>
-    <div className="y-wrap">Header</div>
-  </header>
-);
+import "./header.scss";
+import navs from "./navs.json";
+
+export const Header = (): JSX.Element => {
+  const _navs = navs.map((nav) => <a href="#" key={nav.id}>{nav.text}</a>);
+
+  return (
+    <header className="header">
+      <nav className="header__navs y-wrap">{_navs}</nav>
+    </header>
+  );
+};

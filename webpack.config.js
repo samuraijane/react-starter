661a5a9f37c7c4cd6ea7bde2037090abea6a16fc
Matthew Day <matt@matthewday.net>
2024-02-18 08:36:24 -0500

    load the client view even if the URL is at a nested route

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const common = require("./webpack.common");
 
 module.exports = merge(common, {
   devServer: { // [1]
+    historyApiFallback: true, // [2]
     port: 3000
   },
   plugins: [
@@ -19,4 +20,6 @@ NOTES
   [1] Webpack's dev server bundles files and then performs read/write
       operations in memory so you will never see files run through
       `devServer` written to the disk.
+  [2] This allows the user to refresh at a nested route (e.g. "/about")
+      and still load the page.
 */
